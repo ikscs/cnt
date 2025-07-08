70763531567f26e5cb6054e4f34e2bdf9a355b50
Volodymyr Mykhailov <t380689005276@gmail.com>
2025-07-08 18:13:26 +0300

    Add percentage metric for docker

--- manager/app/docker_metric.py
+++ manager/app/docker_metric.py
@@ -23,7 +23,35 @@ def main(mode, container_name):
             stats = container.stats(stream=False)
             result['cpu_usage'] = stats["cpu_stats"]["cpu_usage"]["total_usage"]
             result['memory_usage'] = stats["memory_stats"]["usage"]
+
+            result['cpu_percentage'] = calculate_cpu_percentage(stats)
+            try:
+                result['memory_percentage'] = round(100*stats["memory_stats"]["usage"] / stats["memory_stats"]["limit"], 1)
+            except Exception as e:
+                result['memory_percentage'] = -1
     return result
+
+def calculate_cpu_percentage(stats):
+    try:
+        # Extract required fields
+        cpu_delta = stats['cpu_stats']['cpu_usage']['total_usage'] - \
+                    stats['precpu_stats']['cpu_usage']['total_usage']
+
+        system_delta = stats['cpu_stats']['system_cpu_usage'] - \
+                       stats['precpu_stats']['system_cpu_usage']
+
+        num_cpus = stats['cpu_stats'].get('online_cpus', 1)  # fallback to 1 to avoid division by 0
+
+        # Avoid division by zero
+        if system_delta > 0 and cpu_delta > 0:
+            cpu_percent = (cpu_delta / system_delta) * num_cpus * 100.0
+        else:
+            cpu_percent = -1
+
+        return round(cpu_percent, 1)
+
+    except (KeyError, ZeroDivisionError, TypeError) as e:
+        return -1
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
