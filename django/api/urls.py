32a0f69ce354a4ddf1d525ae64526c6990317421
Volodymyr Mykhailov <t380689005276@gmail.com>
2025-08-12 20:19:30 +0300

    Reorganize urls

--- django/api/urls.py
+++ django/api/urls.py
@@ -9,82 +9,12 @@ from rest_framework_simplejwt.views import (
 )
 
 from rest_framework.routers import DefaultRouter
-from pcnt.views import AgeViewSet, AppViewSet, BillingViewSet, BillingCostViewSet, BillingIncomeViewSet, CityViewSet, CountryViewSet, CustomerViewSet, CustomerToAppViewSet, DivisionViewSet, EventCrosslineViewSet, EventDataViewSet, FaceDataViewSet, FaceRefererDataViewSet, FaceTimeSlotViewSet, FormViewSet, FormDataViewSet, FormTagViewSet, FormVersionViewSet, IncomingViewSet, ManagerOrderViewSet, MethodViewSet, OriginViewSet, OriginTypeViewSet, OsdViewSet, PermReportViewSet, PersonViewSet, PersonGroupViewSet, PointViewSet
-from pcnt.views import ParamViewSet
-from pcnt.views import HostContainerStatusViewSet, HostDiskUsageViewSet
-from pcnt.views import MetricViewSet, MetricHistoryViewSet
-
-from pcnt.views import LatestMetricViewSet
-from pcnt.views import ExportVCAViewSet
-
-router_pcnt = DefaultRouter()
-
-router_pcnt.register(r'age', AgeViewSet)
-router_pcnt.register(r'app', AppViewSet)
-router_pcnt.register(r'billing', BillingViewSet)
-router_pcnt.register(r'billing_cost', BillingCostViewSet)
-router_pcnt.register(r'billing_income', BillingIncomeViewSet)
-router_pcnt.register(r'city', CityViewSet)
-router_pcnt.register(r'country', CountryViewSet)
-router_pcnt.register(r'customer', CustomerViewSet)
-router_pcnt.register(r'customer_to_app', CustomerToAppViewSet)
-router_pcnt.register(r'division', DivisionViewSet)
-router_pcnt.register(r'event_crossline', EventCrosslineViewSet)
-router_pcnt.register(r'event_data', EventDataViewSet)
-router_pcnt.register(r'face_data', FaceDataViewSet)
-router_pcnt.register(r'face_referer_data', FaceRefererDataViewSet)
-router_pcnt.register(r'face_time_slot', FaceTimeSlotViewSet)
-router_pcnt.register(r'form', FormViewSet)
-router_pcnt.register(r'form_data', FormDataViewSet)
-router_pcnt.register(r'form_tag', FormTagViewSet)
-router_pcnt.register(r'form_version', FormVersionViewSet)
-router_pcnt.register(r'incoming', IncomingViewSet)
-router_pcnt.register(r'manager_order', ManagerOrderViewSet)
-router_pcnt.register(r'method', MethodViewSet)
-router_pcnt.register(r'origin', OriginViewSet)
-router_pcnt.register(r'origin_type', OriginTypeViewSet)
-router_pcnt.register(r'osd', OsdViewSet)
-#router_pcnt.register(r'perm_report', PermReportViewSet)
-router_pcnt.register(r'person', PersonViewSet)
-router_pcnt.register(r'person_group', PersonGroupViewSet)
-router_pcnt.register(r'point', PointViewSet)
-router_pcnt.register(r'param', ParamViewSet)
-router_pcnt.register(r'host_container_status', HostContainerStatusViewSet)
-router_pcnt.register(r'host_disk_usage', HostDiskUsageViewSet)
-router_pcnt.register(r'metric', MetricViewSet)
-router_pcnt.register(r'metric_history', MetricHistoryViewSet)
-router_pcnt.register(r'v_metric_last', LatestMetricViewSet)
-router_pcnt.register(r'v_export_vca', ExportVCAViewSet)
-
-from pcnt.views import FaceRefererByPerson
-from pcnt.views import FaceRefererViewSet
-router_pcnt.register(r'face_referer_data1', FaceRefererViewSet, basename='face-referer')
-
-from pcnt.views import VCustomerOriginViewSet
-router_pcnt.register(r'v_customer_origin', VCustomerOriginViewSet, basename='v-customer-origin')
-
-from pcnt.views import VCustomerPersonViewSet
-router_pcnt.register(r'v_customer_person', VCustomerPersonViewSet, basename='v-customer-person')
-
-from pcnt.views import VCustomerExportViewSet
-router_pcnt.register(r'v_customer_export', VCustomerExportViewSet, basename='v-customer-export')
-
-from pcnt.views import UserCacheViewSet
-router_pcnt.register(r'user_cache', UserCacheViewSet)
-
-from pcnt.views import OriginByPointId
 
 from pcnt.views import CallDbFunctionView
-from .views import HelloView
-
-from pcnt.views import ExportVCAViewSetByPoint
-
 from pcnt.views import CallReportView
-
 from pcnt.views import RegisterCustomerView
 
-perm_report_list = PermReportViewSet.as_view({'get': 'list', 'post': 'create',})
-perm_report_detail = PermReportViewSet.as_view({'get': 'retrieve', 'put': 'update', 'patch': 'partial_update', 'delete': 'destroy',})
+from .views import HelloView
 
 from pcnt.views import ThemeViewSet
 router_api = DefaultRouter()
@@ -92,7 +22,6 @@ router_api.register(r'theme', ThemeViewSet)
 
 urlpatterns = [
     path("api/admin/", admin.site.urls),
-    path("api/pcnt/", include(router_pcnt.urls)),
     path("api/f5", CallDbFunctionView.as_view(), name='call-db-function'),
     path("api/report/", CallReportView.as_view(), name='call-report-view'),
 
@@ -103,17 +32,9 @@ urlpatterns = [
     path('api/authdemo/', include('authdemo.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-#    path('api/pcnt/faces/person/<int:person_id>/', FaceRefererByPerson.as_view(), name='faces-by-person'),
-    path('api/pcnt/face_referer_data/person/<int:person_id>/', FaceRefererByPerson.as_view(), name='faces-by-person'),
-    path('api/pcnt/v_export_vca/point/<int:point_id>/', ExportVCAViewSetByPoint.as_view(), name='v_export_vca-by-point'),
-
-    path('api/pcnt/origin/point/<int:point_id>/', OriginByPointId.as_view(), name='origin-by-point'),
-#    path("api/", include("helloworld.urls")),
-
-    path('api/pcnt/perm_report/', perm_report_list, name='perm_report-list'),
-    path('api/pcnt/perm_report/<str:app_id>/<int:report_id>/', perm_report_detail, name='perm_report-detail'),
 ]
 
+urlpatterns.append(path('api/pcnt/', include('pcnt.urls')))
 urlpatterns.append(path('api/billing/', include('billing.urls')))
 
 if settings.DEBUG:
---
+++ django/pcnt/urls.py
@@ -0,0 +1,75 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from .views import AgeViewSet, AppViewSet, BillingViewSet, BillingCostViewSet, BillingIncomeViewSet, CityViewSet, CountryViewSet, CustomerViewSet, CustomerToAppViewSet, DivisionViewSet, EventCrosslineViewSet, EventDataViewSet, FaceDataViewSet, FaceRefererDataViewSet, FaceTimeSlotViewSet, FormViewSet, FormDataViewSet, FormTagViewSet, FormVersionViewSet, IncomingViewSet, ManagerOrderViewSet, MethodViewSet, OriginViewSet, OriginTypeViewSet, OsdViewSet, PermReportViewSet, PersonViewSet, PersonGroupViewSet, PointViewSet
+from .views import ParamViewSet
+from .views import HostContainerStatusViewSet, HostDiskUsageViewSet
+from .views import MetricViewSet, MetricHistoryViewSet
+
+from .views import LatestMetricViewSet
+from .views import ExportVCAViewSet
+
+from .views import FaceRefererByPerson
+from .views import FaceRefererViewSet
+from .views import VCustomerOriginViewSet
+from .views import VCustomerPersonViewSet
+from .views import VCustomerExportViewSet
+from .views import UserCacheViewSet
+from .views import OriginByPointId
+
+from .views import ExportVCAViewSetByPoint
+
+router = DefaultRouter()
+router.register(r'age', AgeViewSet)
+router.register(r'app', AppViewSet)
+router.register(r'billing', BillingViewSet)
+router.register(r'billing_cost', BillingCostViewSet)
+router.register(r'billing_income', BillingIncomeViewSet)
+router.register(r'city', CityViewSet)
+router.register(r'country', CountryViewSet)
+router.register(r'customer', CustomerViewSet)
+router.register(r'customer_to_app', CustomerToAppViewSet)
+router.register(r'division', DivisionViewSet)
+router.register(r'event_crossline', EventCrosslineViewSet)
+router.register(r'event_data', EventDataViewSet)
+router.register(r'face_data', FaceDataViewSet)
+router.register(r'face_referer_data', FaceRefererDataViewSet)
+router.register(r'face_time_slot', FaceTimeSlotViewSet)
+router.register(r'form', FormViewSet)
+router.register(r'form_data', FormDataViewSet)
+router.register(r'form_tag', FormTagViewSet)
+router.register(r'form_version', FormVersionViewSet)
+router.register(r'incoming', IncomingViewSet)
+router.register(r'manager_order', ManagerOrderViewSet)
+router.register(r'method', MethodViewSet)
+router.register(r'origin', OriginViewSet)
+router.register(r'origin_type', OriginTypeViewSet)
+router.register(r'osd', OsdViewSet)
+router.register(r'person', PersonViewSet)
+router.register(r'person_group', PersonGroupViewSet)
+router.register(r'point', PointViewSet)
+router.register(r'param', ParamViewSet)
+router.register(r'host_container_status', HostContainerStatusViewSet)
+router.register(r'host_disk_usage', HostDiskUsageViewSet)
+router.register(r'metric', MetricViewSet)
+router.register(r'metric_history', MetricHistoryViewSet)
+router.register(r'v_metric_last', LatestMetricViewSet)
+router.register(r'v_export_vca', ExportVCAViewSet)
+
+router.register(r'v_customer_origin', VCustomerOriginViewSet, basename='v-customer-origin')
+router.register(r'v_customer_person', VCustomerPersonViewSet, basename='v-customer-person')
+router.register(r'v_customer_export', VCustomerExportViewSet, basename='v-customer-export')
+router.register(r'user_cache', UserCacheViewSet)
+
+perm_report_list = PermReportViewSet.as_view({'get': 'list', 'post': 'create',})
+perm_report_detail = PermReportViewSet.as_view({'get': 'retrieve', 'put': 'update', 'patch': 'partial_update', 'delete': 'destroy',})
+
+urlpatterns = [
+    path('', include(router.urls)),
+
+    path('face_referer_data/person/<int:person_id>/', FaceRefererByPerson.as_view(), name='faces-by-person'),
+    path('v_export_vca/point/<int:point_id>/', ExportVCAViewSetByPoint.as_view(), name='v_export_vca-by-point'),
+    path('origin/point/<int:point_id>/', OriginByPointId.as_view(), name='origin-by-point'),
+    path('perm_report/', perm_report_list, name='perm_report-list'),
+    path('perm_report/<str:app_id>/<int:report_id>/', perm_report_detail, name='perm_report-detail'),
+]
