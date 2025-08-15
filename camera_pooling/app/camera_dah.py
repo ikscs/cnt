adbf7e9dc5604d7bf45c6bfa244ce0b7e68416c7
Volodymyr <t380689005276@gmail.com>
2025-08-15 18:15:46 +0300

    Create Dahua crossline origin

--- camera_pooling/app/camera_dah.py
+++ camera_pooling/app/camera_dah.py
@@ -59,6 +59,21 @@ class DAH:
         'SummaryNew': 'summary',
       },
 
+      'crossline': {
+        'Channel': 'channel',
+        'Type': 'f_type',
+        'StartTime': 'start_time',
+        'EndTime': 'end_time',
+        'EventTime': 'event_time',
+        'Disk': 'disk',
+        'Cluster': 'cluster',
+        'Events[0]': 'events',
+        'CutLength': 'cut_length',
+        'PicIndex': 'pic_index',
+        'FilePath': 'file_path',
+        'Summary': 'summary',
+      },
+
       'summary': {
         'Object.Image.FilePath': 'image_file_path',
         'Object.Image.Length': 'image_length',
@@ -114,6 +129,11 @@ class DAH:
         'Strabismus': 'strabismus',
 
         'ImageInfo.FilePath': 'file_path',
+
+        'Rule': 'rule',
+        'ObjectType': 'object_type',
+        'Action': 'action',
+
       },
     }
 
@@ -139,6 +159,7 @@ class Camera():
 
         self.req = self._check_session()
         self.is_connected = self.login()
+        #self.is_connected = True
 
     def _check_session(self):
         url = f'{self.url}/magicBox.cgi?action=getHardwareVersion'
@@ -189,7 +210,10 @@ class Camera():
         return r['result']
 
     def get_media(self, media_url):
-        url = f"{self.proto}://{self.host}:{self.port}{media_url}"
+        if 'IntelliStorage' in media_url:
+            url = f"{self.proto}://{self.host}:{self.port}{media_url}"
+        else:
+            url = f"{self.proto}://{self.host}:{self.port}/cgi-bin/RPC_Loadfile{media_url}"
         response = self.req.request('GET', url, timeout=self.timeout)
         return response.content
 
@@ -202,6 +226,7 @@ class Camera():
         return response.status_code
 
     def query(self, q, mode=None):
+        #'''
         url = f'{self.url}/{q}'
         response = self.req.request('GET', url, timeout=self.timeout)
 
@@ -235,7 +260,7 @@ class Camera():
                         k += 1
                     if len(self.items) == idx:
                         self.items.append(self.empty_rec[mode].copy())
-                        if mode in ('recognition', 'detection2'):
+                        if mode in ('recognition', 'detection2', 'crossline'):
                            self.items[idx]['summary'] = dict()
                     key_in = rec[0][k+2:].strip()
                     key_out = self.field_map[mode].get(key_in)
@@ -243,6 +268,11 @@ class Camera():
                         self.items[idx][key_out] = val
                     elif key_in.startswith('SummaryNew[0].Value.'):
                         key_in = key_in[20:]
+                        key_out = self.field_map['summary'].get(key_in)
+                        if key_out:
+                            self.items[idx]['summary'][key_out] = val
+                    elif key_in.startswith('Summary.IVS.'):
+                        key_in = key_in[12:]
                         key_out = self.field_map['summary'].get(key_in)
                         if key_out:
                             self.items[idx]['summary'][key_out] = val
@@ -261,8 +291,8 @@ class Camera():
         self.exec(f"mediaFileFind.cgi?action=destroy&object={self.obj}")
 
     def get_dt(self):
-        start_dt = '2025-05-26 00:00:00'
-        end_dt = '2025-05-27 00:00:00'
+        start_dt = '2025-08-15 17:20:00'
+        end_dt = '2025-08-16 00:00:00'
         return start_dt, end_dt
 
     def get_detection(self, start_dt, end_dt):
@@ -293,14 +323,25 @@ class Camera():
         return_code = self.query(f"mediaFileFind.cgi?action=findFile&{params}")
         if return_code == 200:
             params = f"{self.params}&count={self.max_results}"
-            return_code = self.query(f"mediaFileFind.cgi?action=findNextFile&{params}", 'detection2')
+            return_code = self.query(f"mediaFileFind.cgi?action=findNextFile&{params}", 'detection')
+
+        return return_code
+
+    def get_crossline(self, start_dt, end_dt):
+        params = f"{self.params}&condition.StartTime={start_dt}&condition.EndTime={end_dt}&condition.Types[0]=jpg&condition.DB.IVS.Rule=CrossLineDetection"
+
+        return_code = self.query(f"mediaFileFind.cgi?action=findFile&{params}")
+        if return_code == 200:
+            params = f"{self.params}&count={self.max_results}"
+            return_code = self.query(f"mediaFileFind.cgi?action=findNextFile&{params}", 'crossline')
 
         return return_code
 
 
 if __name__ == "__main__":
     from dotenv import dotenv_values
-    credentials = dotenv_values('.env_dah')
+#    credentials = dotenv_values('.env_dah')
+    credentials = dotenv_values('.env_dah_camera')
     camera = Camera(credentials, max_results=2)
 
     if not camera.is_connected:
@@ -309,13 +350,22 @@ if __name__ == "__main__":
     camera.open_factory()
 
     start_dt, end_dt = camera.get_dt()
-    res = camera.get_detection2(start_dt, end_dt)
+
+    res = camera.get_crossline(start_dt, end_dt)
 
     camera.close_factory()
 
     for e in camera.items:
-        content = camera.get_media(e['summary']['file_path'])
+        content = camera.get_media(e['file_path'])
 
-        image_name = e['summary']['file_path'].split('=')[-1]
-        with open(image_name, 'wb') as fp:
+        if '=' in e['file_path']:
+            file_name = e['file_path'].split('=')[-1]
+        elif '.dav' in e['file_path']:
+            file_name = f"{e['start_time']}__{e['end_time']}.dav".replace(' ', '_').replace(':', '').replace('-', '')
+        elif '.jpg' in e['file_path']:
+            file_name = f"{e['start_time']}.jpg".replace(' ', '_').replace(':', '').replace('-', '')
+        else:
+            file_name = e['file_path'].replace('/', '_')
+
+        with open(file_name, 'wb') as fp:
             fp.write(content)
--- camera_pooling/app/pooling_dah.py
+++ camera_pooling/app/pooling_dah.py
@@ -31,31 +31,49 @@ def dah_runner(credentials, origin, origin_id, last_dt, params):
     end_time = (now.replace(hour=0, minute=0, second=0, microsecond=0) + timedelta(days=1)).strftime("%Y-%m-%d %H:%M:%S")
 
     camera.open_factory()
-    return_code = camera.get_detection2(start_time, end_time)
+    if params['event'] == 'FaceDetection':
+        return_code = camera.get_detection2(start_time, end_time)
+    elif params['event'] == 'CrossLineDetection':
+        return_code = camera.get_crossline(start_time, end_time)
+    else:
+        return 0, None
     camera.close_factory()
 
     if return_code != 200:
         return 0, None
 
     cnt = 0
+    cross_events = []
     for e in camera.items:
-        image_name = get_dict_value(e, 'summary', 'file_path')
+        if params['event'] == 'FaceDetection':
+            image_name = get_dict_value(e, 'summary', 'file_path')
 
-        title = dict()
-        for param in ('sex', 'age', 'glasses', 'emotion', 'mask', 'beard', 'nation'):
-            title[param] = get_dict_value(e, 'summary', param)
-        for param in ('glasses', 'mask', 'beard'):
-            title[param] = {'0': None, '1': False, '2': True}.get(title[param])
+            title = dict()
+            for param in ('sex', 'age', 'glasses', 'emotion', 'mask', 'beard', 'nation'):
+                title[param] = get_dict_value(e, 'summary', param)
+            for param in ('glasses', 'mask', 'beard'):
+                title[param] = {'0': None, '1': False, '2': True}.get(title[param])
 
-        if image_name:
-            #end_event = get_dict_value(e, 'event_time')
+            if image_name:
+                #end_event = get_dict_value(e, 'event_time')
+                end_time = get_dict_value(e, 'end_time')
+                content = camera.get_media(image_name)
+                image_name = image_name.split('=')[-1]
+                se.checkin(origin, origin_id, json.dumps(title), image_name, content, end_time)
+                cnt += 1
+            else:
+                return 0, None
+
+        elif params['event'] == 'CrossLineDetection':
             end_time = get_dict_value(e, 'end_time')
-            content = camera.get_media(image_name)
-            image_name = image_name.split('=')[-1]
-            se.checkin(origin, origin_id, json.dumps(title), image_name, content, end_time)
+            file_name = e['file_path']
+            content = camera.get_media(file_name)
+            cross_events.append({'origin': origin, 'origin_id': origin_id, 'ts': end_time, 'prefix': e['events'], 'name': file_name})
+            se.checkin(origin, origin_id, e['events'], file_name, content, end_time)
             cnt += 1
-        else:
-            return 0, None
+
+    if cross_events:
+        se.upload_events(cross_events)
 
     logging.info(f"Parsed {cnt}. Last event {end_time}")
     return cnt, end_time
@@ -64,12 +82,14 @@ if __name__ == "__main__":
     import os
     from dotenv import dotenv_values
 
-    credentials = dotenv_values('.env_dah')
-    origin = 'sezon_DAH_1'
-    origin_id = 21
-    last_dt = datetime.now() + timedelta(seconds=-36000)
+#    credentials = dotenv_values('.env_dah')
+    credentials = dotenv_values('.env_dah_camera')
+    origin = 'office_hik_camera'
+    origin_id = 45
+    last_dt = datetime.now() + timedelta(seconds=-3600)
 
-    params = None
+#    params = {'event': 'FaceDetection'}
+    params = {'event': 'CrossLineDetection'}
 
     count, end_time = dah_runner(credentials, origin, origin_id, last_dt, params)
     print(count, end_time)
