a4dea0bf55aa0b4e899cb38dcc71867c26da8856
Volodymyr Mykhailov <t380689005276@gmail.com>
2025-08-19 13:33:17 +0300

    Fix err in CreateLiqpayOrder

--- django/billing/views.py
+++ django/billing/views.py
@@ -202,13 +202,14 @@ class SubscriptionBasePriceViewSet(PCNTBaseReadOnlyViewSet):
 
 class CreateLiqpayOrderView(PCNTBaseAPIView):
     def post(self, request, *args, **kwargs):
-        amount = request.POST.get('amount')
-        currency = request.POST.get('currency')
-        description = request.POST.get('description')
-        app_id = request.POST.get('app_id')
-        param = request.POST.get('param')
+        data = request.data
+        amount = data.get('amount')
+        currency = data.get('currency')
+        description = data.get('description')
+        app_id = data.get('app_id')
+        param = data.get('param')
 
-        if not all((amount, currency, description, app_id, (param or param == {}):
+        if not all((amount, currency, description, app_id, not(param is None))):
             return Response({'error': 'Data incomplete'}, status=status.HTTP_400_BAD_REQUEST)
 
         user = request.user
@@ -216,7 +217,7 @@ class CreateLiqpayOrderView(PCNTBaseAPIView):
         with connections['pcnt'].cursor() as cursor:
             query = f'SELECT * FROM billing.create_liqpay_order(%s, %s, %s, %s, %s, %s, %s, %s);'
             try:
-                cursor.execute(query, [amount, currency, description, app_id, user.tenant_id, user.id, user.mode, param])
+                cursor.execute(query, [amount, currency, description, app_id, user.tenant_id, user.id, user.mode, json.dumps(param)])
                 row = cursor.fetchone()
                 if not row or not row[0]:
                     return Response({'error': f'Order not created'}, status=status.HTTP_400_BAD_REQUEST)
