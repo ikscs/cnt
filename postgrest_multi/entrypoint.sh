8a26df3c947cc8650eb79ced965a2c28934f93ab
Volodymyr Mykhailov <t380689005276@gmail.com>
2025-12-01 19:44:39 +0200

    Create pre_config for postgREST

--- postgrest_multi/entrypoint.sh
+++ postgrest_multi/entrypoint.sh
@@ -11,6 +11,7 @@ for api in "${INSTANCES[@]}"; do
   var_anon_role=$(echo "${api}_DB_ANON_ROLE" | tr '[:lower:]' '[:upper:]')
   var_jwt_secret=$(echo "${api}_JWT_secret" | tr '[:lower:]' '[:upper:]')
   var_jwt_role_claim_key=$(echo "${api}_JWT_ROLE_CLAIM_KEY" | tr '[:lower:]' '[:upper:]')
+  var_db_pre_config=$(echo "${api}_DB_PRE_CONFIG" | tr '[:lower:]' '[:upper:]')
   var_db_pre_request=$(echo "${api}_DB_PRE_REQUEST" | tr '[:lower:]' '[:upper:]')
 
   # Expand environment variables
@@ -20,12 +21,13 @@ for api in "${INSTANCES[@]}"; do
   anon_role="${!var_anon_role}"
   jwt_secret="${!var_jwt_secret}"
   jwt_role_claim_key="${!var_jwt_role_claim_key}"
+  db_pre_config="${!var_db_pre_config}"
   db_pre_request="${!var_db_pre_request}"
 
   # Generate config
   conf_file="/etc/postgrest/${api}.conf"
   echo "Generating config for $api (port $port) (schemas $schemas)"
-  DB_URI="$db_uri" DB_SCHEMAS="$schemas" DB_ANON_ROLE="$anon_role" JWT_ROLE_CLAIM_KEY="$jwt_role_claim_key" JWT_SECRET="$jwt_secret" DB_PRE_REQUEST="$db_pre_request" SERVER_PORT="$port" envsubst < /etc/postgrest/api.template.conf > "$conf_file"
+  DB_URI="$db_uri" DB_SCHEMAS="$schemas" DB_ANON_ROLE="$anon_role" JWT_ROLE_CLAIM_KEY="$jwt_role_claim_key" JWT_SECRET="$jwt_secret" DB_PRE_CONFIG="$db_pre_config" DB_PRE_REQUEST="$db_pre_request" SERVER_PORT="$port" envsubst < /etc/postgrest/api.template.conf > "$conf_file"
 
   # Start PostgREST instance
   postgrest "$conf_file" &
